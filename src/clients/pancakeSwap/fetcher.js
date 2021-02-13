bdb059cee56a808cd673756d8afba84cc82fd764
Lukas <6736344+lukaskai@users.noreply.github.com>
2021-02-13 08:49:03 +0200

    Modified calculations and implemented aggregation

--- src/clients/pancakeSwap/fetcher.js
+++ src/clients/pancakeSwap/fetcher.js
@@ -5,15 +5,18 @@ import farmsConfig from './constants/farms';
 import addresses from './constants/contracts';
 import multicall from './multicall';
 
+const displayDecimals = 5;
+
 export const getAddress = (address) => {
   const mainNetChainId = 56;
   return address[mainNetChainId];
 };
 
 const getMasterChefAddress = () => getAddress(addresses.masterChef);
-const getFullDisplayBalance = (balance, decimals) => balance.dividedBy(new BigNumber(10).pow(decimals)).toFixed();
+const getFullDisplayBalance = (balance, decimals) => Number(balance.dividedBy(new BigNumber(10)
+  .pow(decimals)).toFixed(displayDecimals));
 
-export const fetchFarmUserTokenBalances = async (account) => {
+export const fetchLpTokenBalances = async (account) => {
   const calls = farmsConfig.map((farm) => {
     const lpContractAddress = getAddress(farm.lpAddresses);
     return {
@@ -23,9 +26,21 @@ export const fetchFarmUserTokenBalances = async (account) => {
     };
   });
 
+  const callsTokens = farmsConfig.map((farm) => ({
+    address: getAddress(farm.lpAddresses),
+    name: 'decimals',
+  }));
+
+  const tokenDecimals = await multicall(ERC20, callsTokens);
+
   const rawTokenBalances = await multicall(ERC20, calls);
-  const parsedTokenBalances = rawTokenBalances.map((tokenBalance) => new BigNumber(tokenBalance).toJSON());
-  return parsedTokenBalances;
+  const parsedLpTokenBalances = rawTokenBalances.map((tokenBalance, index) => ({
+    balanceRaw: new BigNumber(tokenBalance).toJSON(),
+    decimals: tokenDecimals[index][0],
+    displayBalance: getFullDisplayBalance(new BigNumber(tokenBalance), tokenDecimals[index][0]),
+  }));
+
+  return parsedLpTokenBalances;
 };
 
 export const fetchFarmUserStakedBalances = async (account) => {
@@ -40,7 +55,7 @@ export const fetchFarmUserStakedBalances = async (account) => {
   const rawStakedBalances = await multicall(MasterChef, calls);
 
   const callsTokens = farmsConfig.map((farm) => ({
-    address: getAddress(farm.tokenAddresses),
+    address: getAddress(farm.lpAddresses),
     name: 'decimals',
   }));
 
@@ -54,16 +69,95 @@ export const fetchFarmUserStakedBalances = async (account) => {
   return parsedStakedBalances;
 };
 
-export const fetchFarmUserEarnings = async (account) => {
-  const masterChefAdress = getMasterChefAddress();
+// export const fetchFarmUserEarnings = async (account) => {
+//   const masterChefAdress = getMasterChefAddress();
+//
+//   const calls = farmsConfig.map((farm) => ({
+//     address: masterChefAdress,
+//     name: 'pendingCake',
+//     params: [farm.pid, account],
+//   }));
+//
+//   const callsTokens = farmsConfig.map((farm) => ({
+//     address: getAddress(farm.tokenAddresses),
+//     name: 'decimals',
+//   }));
+//
+//   const tokenDecimals = await multicall(ERC20, callsTokens);
+//
+//   const rawEarnings = await multicall(MasterChef, calls);
+//   const parsedEarnings = rawEarnings.map((rawEarning, index) => ({
+//     earningsRaw: new BigNumber(rawEarning).toJSON(),
+//     decimals: tokenDecimals[index][0],
+//     displayBalance: getFullDisplayBalance(new BigNumber(rawEarning), tokenDecimals[index][0]),
+//   }));
+//   return parsedEarnings;
+// };
 
-  const calls = farmsConfig.map((farm) => ({
-    address: masterChefAdress,
-    name: 'pendingCake',
-    params: [farm.pid, account],
-  }));
 
-  const rawEarnings = await multicall(MasterChef, calls);
-  const parsedEarnings = rawEarnings.map((earnings) => new BigNumber(earnings).toJSON());
-  return parsedEarnings;
+export const fetchFarms = async () => {
+  const data = await Promise.all(
+    farmsConfig.map(async (farmConfig) => {
+      const lpAdress = getAddress(farmConfig.lpAddresses);
+      const calls = [
+        // Balance of token in the LP contract
+        {
+          address: getAddress(farmConfig.tokenAddresses),
+          name: 'balanceOf',
+          params: [lpAdress],
+        },
+        // Balance of quote token on LP contract
+        {
+          address: getAddress(farmConfig.quoteTokenAdresses),
+          name: 'balanceOf',
+          params: [lpAdress],
+        },
+        // Balance of LP tokens in the master chef contract
+        {
+          address: lpAdress,
+          name: 'balanceOf',
+          params: [getMasterChefAddress()],
+        },
+        // Total supply of LP tokens
+        {
+          address: lpAdress,
+          name: 'totalSupply',
+        },
+        // Token decimals
+        {
+          address: getAddress(farmConfig.tokenAddresses),
+          name: 'decimals',
+        },
+        // Quote token decimals
+        {
+          address: getAddress(farmConfig.quoteTokenAdresses),
+          name: 'decimals',
+        },
+        // Lp token decimals
+        {
+          address: lpAdress,
+          name: 'decimals',
+        },
+      ];
+
+      const [
+        tokenBalanceLP,
+        quoteTokenBlanceLP,
+        lpTokenBalanceMC,
+        lpTotalSupply,
+        tokenDecimals,
+        quoteTokenDecimals,
+        lpTokenDecimals,
+      ] = await multicall(ERC20, calls);
+
+
+      return {
+        ...farmConfig,
+        underlyingTokenSupply: getFullDisplayBalance(new BigNumber(tokenBalanceLP), tokenDecimals),
+        underlyingQuoteTokenSupply: getFullDisplayBalance(new BigNumber(quoteTokenBlanceLP), quoteTokenDecimals),
+        lpTotalSupply: getFullDisplayBalance(new BigNumber(lpTotalSupply), lpTokenDecimals),
+      };
+    }),
+  );
+  return data;
 };
--- src/services/assets.js
+++ src/services/assets.js
@@ -1,7 +1,6 @@
 import {
-  fetchFarmUserTokenBalances,
-  fetchFarmUserStakedBalances,
-  fetchFarmUserEarnings,
+  fetchLpTokenBalances,
+  fetchFarmUserStakedBalances, fetchFarms,
 } from '../clients/pancakeSwap/fetcher';
 import farmsConfig from '../clients/pancakeSwap/constants/farms';
 
@@ -10,26 +9,80 @@ export const getAddress = (address) => {
   return address[mainNetChainId];
 };
 
+function getLiquidityValue(
+  underlyingTokenReserve,
+  totalLpTokenSupply,
+  userLpTokenLiquidity,
+) {
+  return (userLpTokenLiquidity * underlyingTokenReserve) / totalLpTokenSupply;
+}
+
 export default {
   async getAssets(address) {
-    // const userFarmTokenBalances = await fetchFarmUserTokenBalances(address);
+    const lpTokensUnderlying = await fetchFarms();
+    const userLpTokenBalances = await fetchLpTokenBalances(address);
     const userStakedBalances = await fetchFarmUserStakedBalances(address);
-    // const userFarmEarnings = await fetchFarmUserEarnings(address);
+    const rawData = {};
+
+    userStakedBalances.forEach((balances, index) => {
+      if (userStakedBalances[index].displayBalance > 0
+          || userLpTokenBalances[index].displayBalance) {
+        rawData[farmsConfig[index].lpSymbol] = {
+          tokenSymbol: farmsConfig[index].tokenSymbol,
+          quoteTokenSymbol: farmsConfig[index].quoteTokenSymbol,
+          lpSymbol: farmsConfig[index].lpSymbol,
+          tokenAddress: getAddress(farmsConfig[index].tokenAddresses),
+          quoteTokenAddress: getAddress(farmsConfig[index].quoteTokenAdresses),
+          stakedLpTokenBalance: userStakedBalances[index].displayBalance,
+          lpTokenBalances: userLpTokenBalances[index].displayBalance,
+          totalLpTokenBalance: userLpTokenBalances[index].displayBalance + userStakedBalances[index].displayBalance,
+        };
+
+        rawData[farmsConfig[index].lpSymbol].totalUnderlyingTokenBalance = getLiquidityValue(
+          lpTokensUnderlying[index].underlyingTokenSupply,
+          lpTokensUnderlying[index].lpTotalSupply,
+          rawData[farmsConfig[index].lpSymbol].totalLpTokenBalance,
+        );
+
+        rawData[farmsConfig[index].lpSymbol].totalUnderlyingQuoteTokenBalance = getLiquidityValue(
+          lpTokensUnderlying[index].underlyingQuoteTokenSupply,
+          lpTokensUnderlying[index].lpTotalSupply,
+          rawData[farmsConfig[index].lpSymbol].totalLpTokenBalance,
+        );
+      }
+    });
+
 
     const underlying = {};
 
-    userStakedBalances.forEach((balances, index) => {
-      underlying[farmsConfig[index].tokenSymbol] = {
-        tokenAddress: getAddress(farmsConfig[index].tokenAddresses),
-        symbol: farmsConfig[index].tokenSymbol,
-        stakedBalance: userStakedBalances[index],
-      };
+    Object.keys(rawData).forEach((dataKey) => {
+      const entry = rawData[dataKey];
+      if (underlying[entry.tokenSymbol]) {
+        underlying[entry.tokenSymbol].amount += entry.totalUnderlyingTokenBalance;
+      } else {
+        underlying[entry.tokenSymbol] = {
+          amount: entry.totalUnderlyingTokenBalance,
+          symbol: entry.tokenSymbol,
+          tokenAddress: entry.tokenAddress,
+        };
+      }
+      if (underlying[entry.quoteTokenSymbol]) {
+        underlying[entry.quoteTokenSymbol].amount += entry.totalUnderlyingQuoteTokenBalance;
+      } else {
+        underlying[entry.quoteTokenSymbol] = {
+          amount: entry.totalUnderlyingQuoteTokenBalance,
+          symbol: entry.quoteTokenSymbol,
+          tokenAddress: entry.quoteTokenAddress,
+        };
+      }
     });
+
 
     return {
       asset: {
         PancakeSwap: {
           underlying,
+          rawData,
         },
       },
       walletId: address,
